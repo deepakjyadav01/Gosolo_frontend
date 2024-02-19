d82cc59e1c0818609f644c9193e924499a804bad
Deepak Yadav <82588792+deepakjyadav01@users.noreply.github.com>
2024-02-19 13:37:06 +0000

    Update ChatWindow.jsx

--- src/Components/chat/ChatWindow.jsx
+++ src/Components/chat/ChatWindow.jsx
@@ -5,7 +5,7 @@ import ScrollableFeed from 'react-scrollable-feed'
 import io from 'socket.io-client';
 //http://localhost:3200
 //https://gosolo-api.onrender.com/
-const socket = io('http://localhost:3200');
+const socket = io('https://gosolo-api.onrender.com/');
 
 function ChatWindow() {
   const location = useLocation();
